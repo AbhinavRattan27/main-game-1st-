306845f9c2e883021a5328105cd73d9f246f1d74
AbhinavRattan27 <83867719+AbhinavRattan27@users.noreply.github.com>
2022-01-23 13:49:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+var mine = Math.floor(Math.random() * 10) + 1;
+
+function lfthnd(){
+  var mine = Math.floor(Math.random() * 10) + 1;
+  document.getElementById('varh5').innerHTML= mine;
+  if (mine= 1){
+    document.getElementById('hekkin').innerHTML="left";
+    }
+  if (mine = 3){
+    document.getElementById('hekkin').innerHTML="left";
+    }
+  if (mine = 5){
+    document.getElementById('hekkin').innerHTML="left";
+    }
+  if (mine = 7){
+    document.getElementById('hekkin').innerHTML="left";
+    }
+  if (mine = 9){
+    document.getElementById('hekkin').innerHTML="left";
+    }
+    if (mine = 2){
+      document.getElementById('hekkin').innerHTML="right";
+      }
+      if (mine = 4){
+      document.getElementById('hekkin').innerHTML="right";
+      }
+      if (mine = 6){
+      document.getElementById('hekkin').innerHTML="right";
+      }
+      if (mine = 8){
+      document.getElementById('hekkin').innerHTML="right";
+      }
+      if (mine = 10){
+      document.getElementById('hekkin').innerHTML="right";
+      }
+  }
+function rhthnd(){
+  var mine = Math.floor(Math.random() * 10) + 1;
+  document.getElementById("varh5").innerHTML= mine;
+    if (mine = 2){
+    document.getElementById("hekkin").innerHTML="right";
+    }
+    if (mine = 4){
+    document.getElementById("hekkin").innerHTML="right";
+    }
+    if (mine = 6){
+    document.getElementById("hekkin").innerHTML="right";
+    }
+    if (mine = 8){
+    document.getElementById("hekkin").innerHTML="right";
+    }
+    if (mine = 10){
+    document.getElementById("hekkin").innerHTML="right";
+    }
+    if (mine= 1){
+      document.getElementById("hekkin").innerHTML="left";
+      }
+    if (mine = 3){
+      document.getElementById("hekkin").innerHTML="left";
+      }
+    if (mine = 5){
+      document.getElementById("hekkin").innerHTML="left";
+      }
+    if (mine = 7){
+      document.getElementById("hekkin").innerHTML="left";
+      }
+    if (mine = 9){
+      document.getElementById("hekkin").innerHTML="left";
+      }
+  
+}
+
+
+
+   
+
+
+
+  
